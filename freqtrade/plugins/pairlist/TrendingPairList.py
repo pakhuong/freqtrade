6562ff0e3b2aa336f952f8e7a82835206aa22a31
Khuong Pham <pakhuong@users.noreply.github.com>
2025-01-28 17:39:28 +0700

    feat: used GMMA to determine trend direction

--- freqtrade/plugins/pairlist/TrendingPairList.py
+++ freqtrade/plugins/pairlist/TrendingPairList.py
@@ -8,6 +8,7 @@ import logging
 from datetime import timedelta
 from typing import Any, Literal, Optional
 
+import talib.abstract as ta
 from cachetools import TTLCache
 
 from freqtrade.constants import ListPairsWithTimeframes
@@ -21,9 +22,6 @@ from freqtrade.util import dt_now, format_ms_time
 logger = logging.getLogger(__name__)
 
 
-SORT_VALUES = ["trendStrength"]
-
-
 class TrendingPairList(IPairList):
     is_pairlist_generator = True
     supports_backtesting = SupportsBacktesting.NO
@@ -31,37 +29,11 @@ class TrendingPairList(IPairList):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        if "number_assets" not in self._pairlistconfig:
-            raise OperationalException(
-                "`number_assets` not specified. Please check your configuration "
-                'for "pairlist.config.number_assets"'
-            )
-
         self._stake_currency = self._config["stake_currency"]
-        self._number_pairs = self._pairlistconfig["number_assets"]
-        self._sort_key: Literal["trendStrength"] = self._pairlistconfig.get(
-            "sort_key", "trendStrength"
-        )
         self._refresh_period = self._pairlistconfig.get("refresh_period", 1800)
         self._pair_cache: TTLCache = TTLCache(maxsize=1, ttl=self._refresh_period)
-        self._ma_period = self._pairlistconfig.get("ma_period", 10)
-        self._lookback_days = self._pairlistconfig.get("lookback_days", 0)
         self._lookback_timeframe = self._pairlistconfig.get("lookback_timeframe", "1d")
-        self._lookback_period = self._pairlistconfig.get("lookback_period", 0)
-        self._sort_direction: Optional[str] = self._pairlistconfig.get("sort_direction", "desc")
         self._def_candletype = self._config["candle_type_def"]
-
-        if (self._lookback_days > 0) & (self._lookback_period > 0):
-            raise OperationalException(
-                "Ambiguous configuration: lookback_days and lookback_period both set in pairlist "
-                "config. Please set lookback_days only or lookback_period and lookback_timeframe "
-                "and restart the bot."
-            )
-
-        # overwrite lookback timeframe and days when lookback_days is set
-        if self._lookback_days > 0:
-            self._lookback_timeframe = "1d"
-            self._lookback_period = self._lookback_days
 
         # get timeframe in minutes and seconds
         self._tf_in_min = timeframe_to_minutes(self._lookback_timeframe)
@@ -87,22 +59,6 @@ class TrendingPairList(IPairList):
                 "or switch to using candles. and restart the bot."
             )
 
-        if not self._validate_keys(self._sort_key):
-            raise OperationalException(f"key {self._sort_key} not in {SORT_VALUES}")
-
-        candle_limit = self._exchange.ohlcv_candle_limit(
-            self._lookback_timeframe, self._config["candle_type_def"]
-        )
-
-        if self._ma_period <= 0:
-            raise OperationalException("TrendFilter requires ma_period to be > 0")
-
-        if self._ma_period > candle_limit:
-            raise OperationalException(
-                "TrendFilter requires ma_period to not "
-                f"exceed exchange max request size ({candle_limit})"
-            )
-
     @property
     def needstickers(self) -> bool:
         """
@@ -112,66 +68,25 @@ class TrendingPairList(IPairList):
         """
         return not self._use_range
 
-    def _validate_keys(self, key):
-        return key in SORT_VALUES
-
     def short_desc(self) -> str:
         """
         Short whitelist method description - used for startup-messages
         """
-        return f"{self.name} - top {self._pairlistconfig['number_assets']} trending pairs."
+        return f"{self.name} - top trending pairs."
 
     @staticmethod
     def description() -> str:
-        return "Provides dynamic pair list based on trend strength."
+        return "Provides dynamic trending pair list based on GMMA."
 
     @staticmethod
     def available_parameters() -> dict[str, PairlistParameter]:
         return {
-            "number_assets": {
-                "type": "number",
-                "default": 30,
-                "description": "Number of assets",
-                "help": "Number of assets to use from the pairlist",
-            },
-            "ma_period": {
-                "type": "number",
-                "default": 10,
-                "description": "MA Period",
-                "help": "Moving average period to use for trend strength calculation.",
-            },
-            "sort_key": {
-                "type": "option",
-                "default": "trendStrength",
-                "options": SORT_VALUES,
-                "description": "Sort key",
-                "help": "Sort key to use for sorting the pairlist.",
-            },
-            "sort_direction": {
-                "type": "option",
-                "default": "desc",
-                "options": ["", "asc", "desc"],
-                "description": "Sort pairlist",
-                "help": "Sort Pairlist ascending or descending by rate of change.",
-            },
             **IPairList.refresh_period_parameter(),
-            "lookback_days": {
-                "type": "number",
-                "default": 0,
-                "description": "Lookback Days",
-                "help": "Number of days to look back at.",
-            },
             "lookback_timeframe": {
                 "type": "string",
                 "default": "",
                 "description": "Lookback Timeframe",
                 "help": "Timeframe to use for lookback.",
-            },
-            "lookback_period": {
-                "type": "number",
-                "default": 0,
-                "description": "Lookback Period",
-                "help": "Number of periods to look back at.",
             },
         }
 
@@ -184,6 +99,7 @@ class TrendingPairList(IPairList):
         # Generate dynamic whitelist
         # Must always run if this pairlist is not the first in the list.
         pairlist = self._pair_cache.get("pairlist")
+
         if pairlist:
             # Item found - no refresh necessary
             return pairlist.copy()
@@ -196,8 +112,10 @@ class TrendingPairList(IPairList):
                     quote_currencies=[self._stake_currency], tradable_only=True, active_only=True
                 ).keys()
             ]
+
             # No point in testing for blacklisted pairs...
             _pairlist = self.verify_blacklist(_pairlist, logger.info)
+
             if not self._use_range:
                 filtered_tickers = [
                     v
@@ -208,6 +126,7 @@ class TrendingPairList(IPairList):
                         and v["symbol"] in _pairlist
                     )
                 ]
+
                 pairlist = [s["symbol"] for s in filtered_tickers]
             else:
                 pairlist = _pairlist
@@ -225,83 +144,115 @@ class TrendingPairList(IPairList):
         :param tickers: Tickers (from exchange.get_tickers). May be cached.
         :return: new whitelist
         """
-        if self._use_range:
-            # Create bare minimum from tickers structure.
-            filtered_tickers: list[dict[str, Any]] = [{"symbol": k} for k in pairlist]
 
-            # get lookback period in ms, for exchange ohlcv fetch
-            since_ms = (
-                int(
-                    timeframe_to_prev_date(
-                        self._lookback_timeframe,
-                        dt_now()
-                        + timedelta(
-                            minutes=-((self._ma_period + self._lookback_period) * self._tf_in_min)
-                            - self._tf_in_min
-                        ),
-                    ).timestamp()
-                )
-                * 1000
+        # Create bare minimum from tickers structure.
+        filtered_tickers: list[dict[str, Any]] = [{"symbol": k} for k in pairlist]
+
+        # get lookback period in ms, for exchange ohlcv fetch
+        since_ms = (
+            int(
+                timeframe_to_prev_date(
+                    self._lookback_timeframe,
+                    dt_now() + timedelta(minutes=-(250 * self._tf_in_min) - self._tf_in_min),
+                ).timestamp()
             )
-
-            to_ms = (
-                int(
-                    timeframe_to_prev_date(
-                        self._lookback_timeframe, dt_now() - timedelta(minutes=self._tf_in_min)
-                    ).timestamp()
-                )
-                * 1000
-            )
-
-            # todo: utc date output for starting date
-            self.log_once(
-                f"Using trading range of {self._ma_period + self._lookback_period} candles, timeframe: "
-                f"{self._lookback_timeframe}, starting from {format_ms_time(since_ms)} "
-                f"till {format_ms_time(to_ms)}",
-                logger.info,
-            )
-            needed_pairs: ListPairsWithTimeframes = [
-                (p, self._lookback_timeframe, self._def_candletype)
-                for p in [s["symbol"] for s in filtered_tickers]
-                if p not in self._pair_cache
-            ]
-
-            candles = self._exchange.refresh_ohlcv_with_cache(needed_pairs, since_ms)
-
-            for i, p in enumerate(filtered_tickers):
-                pair_candles = (
-                    candles[(p["symbol"], self._lookback_timeframe, self._def_candletype)]
-                    if (p["symbol"], self._lookback_timeframe, self._def_candletype) in candles
-                    else None
-                )
-
-                # in case of candle data calculate simple moving average and trend strength
-                if pair_candles is not None and not pair_candles.empty:
-                    pair_candles["ma"] = pair_candles["close"].rolling(self._ma_period).mean()
-
-                    pair_candles["trendStrength"] = abs(
-                        (pair_candles["ma"] - pair_candles["ma"].shift(self._lookback_period))
-                        / pair_candles["ma"].shift(self._lookback_period)
-                        * 100
-                    )
-
-                    filtered_tickers[i]["trendStrength"] = pair_candles["trendStrength"].iloc[-1]
-                else:
-                    filtered_tickers[i]["trendStrength"] = 0
-        else:
-            # Tickers mode - filter based on incoming pairlist.
-            filtered_tickers = [v for k, v in tickers.items() if k in pairlist]
-
-        sorted_tickers = sorted(
-            filtered_tickers,
-            reverse=self._sort_direction == "desc",
-            key=lambda t: t[self._sort_key],
+            * 1000
         )
 
+        to_ms = (
+            int(
+                timeframe_to_prev_date(
+                    self._lookback_timeframe, dt_now() - timedelta(minutes=self._tf_in_min)
+                ).timestamp()
+            )
+            * 1000
+        )
+
+        self.log_once(
+            f"Using trading range of 250 candles, timeframe: "
+            f"{self._lookback_timeframe}, starting from {format_ms_time(since_ms)} "
+            f"till {format_ms_time(to_ms)}",
+            logger.info,
+        )
+
+        needed_pairs: ListPairsWithTimeframes = [
+            (p, self._lookback_timeframe, self._def_candletype)
+            for p in [s["symbol"] for s in filtered_tickers]
+            if p not in self._pair_cache
+        ]
+
+        candles = self._exchange.refresh_ohlcv_with_cache(needed_pairs, since_ms)
+
+        for i, p in enumerate(filtered_tickers):
+            pair_candles = (
+                candles[(p["symbol"], self._lookback_timeframe, self._def_candletype)]
+                if (p["symbol"], self._lookback_timeframe, self._def_candletype) in candles
+                else None
+            )
+
+            # in case of candle data calculate simple moving average and trend strength
+            if pair_candles is not None and not pair_candles.empty:
+                pair_candles["ema3"] = ta.EMA(pair_candles, timeperiod=3)
+                pair_candles["ema5"] = ta.EMA(pair_candles, timeperiod=5)
+                pair_candles["ema8"] = ta.EMA(pair_candles, timeperiod=8)
+                pair_candles["ema10"] = ta.EMA(pair_candles, timeperiod=10)
+                pair_candles["ema12"] = ta.EMA(pair_candles, timeperiod=12)
+                pair_candles["ema15"] = ta.EMA(pair_candles, timeperiod=15)
+                pair_candles["ema30"] = ta.EMA(pair_candles, timeperiod=30)
+                pair_candles["ema35"] = ta.EMA(pair_candles, timeperiod=35)
+                pair_candles["ema40"] = ta.EMA(pair_candles, timeperiod=40)
+                pair_candles["ema45"] = ta.EMA(pair_candles, timeperiod=45)
+                pair_candles["ema50"] = ta.EMA(pair_candles, timeperiod=50)
+                pair_candles["ema60"] = ta.EMA(pair_candles, timeperiod=60)
+                pair_candles["trend_direction"] = 0
+
+                pair_candles.loc[
+                    (
+                        (pair_candles["ema3"] > pair_candles["ema5"])
+                        & (pair_candles["ema5"] > pair_candles["ema8"])
+                        & (pair_candles["ema8"] > pair_candles["ema10"])
+                        & (pair_candles["ema10"] > pair_candles["ema12"])
+                        & (pair_candles["ema12"] > pair_candles["ema15"])
+                        & (pair_candles["ema15"] > pair_candles["ema30"])
+                        & (pair_candles["ema30"] > pair_candles["ema35"])
+                        & (pair_candles["ema35"] > pair_candles["ema40"])
+                        & (pair_candles["ema40"] > pair_candles["ema45"])
+                        & (pair_candles["ema45"] > pair_candles["ema50"])
+                        & (pair_candles["ema50"] > pair_candles["ema60"])
+                    ),
+                    "trend_direction",
+                ] = 1
+
+                pair_candles.loc[
+                    (
+                        (pair_candles["ema3"] < pair_candles["ema5"])
+                        & (pair_candles["ema5"] < pair_candles["ema8"])
+                        & (pair_candles["ema8"] < pair_candles["ema10"])
+                        & (pair_candles["ema10"] < pair_candles["ema12"])
+                        & (pair_candles["ema12"] < pair_candles["ema15"])
+                        & (pair_candles["ema15"] < pair_candles["ema30"])
+                        & (pair_candles["ema30"] < pair_candles["ema35"])
+                        & (pair_candles["ema35"] < pair_candles["ema40"])
+                        & (pair_candles["ema40"] < pair_candles["ema45"])
+                        & (pair_candles["ema45"] < pair_candles["ema50"])
+                        & (pair_candles["ema50"] < pair_candles["ema60"])
+                    ),
+                    "trend_direction",
+                ] = -1
+
+                filtered_tickers[i]["trendDirection"] = pair_candles["trend_direction"].iloc[-1]
+            else:
+                filtered_tickers[i]["trendDirection"] = 0
+
         # Validate whitelist to only have active market pairs
-        pairs = self._whitelist_for_active_markets([s["symbol"] for s in sorted_tickers])
+        pairs = self._whitelist_for_active_markets([s["symbol"] for s in filtered_tickers])
         pairs = self.verify_blacklist(pairs, logmethod=logger.info)
-        # Limit pairlist to the requested number of pairs
-        pairs = pairs[: self._number_pairs]
+
+        # Limit pairlist to the trending pairs
+        pairs = [
+            s["symbol"]
+            for s in filtered_tickers
+            if s["symbol"] in pairs and (s["trendDirection"] == 1 or s["trendDirection"] == -1)
+        ]
 
         return pairs
